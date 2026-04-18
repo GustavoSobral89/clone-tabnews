3ab6f22cd415b1af7c9a63196392ec2e627688ea
GustavoSobral89 <39865263+GustavoSobral89@users.noreply.github.com>
2026-04-18 00:34:56 +0000

    fix: change `join` to `resolve` in migrations

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -7,7 +7,7 @@ beforeAll(async () => {
 
 describe("GET to /api/v1/migrations", () => {
   describe("Anonymous user", () => {
-    test("Running pending migrations", async () => {
+    test("Retreiving pending migrations", async () => {
       const response = await fetch("http://localhost:3000/api/v1/migrations");
       expect(response.status).toBe(200);
 
