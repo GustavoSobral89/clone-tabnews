1179f182485b6b783a774f8d25f87e5aff636aa4
GustavoSobral89 <39865263+GustavoSobral89@users.noreply.github.com>
2026-06-02 11:51:34 +0000

    feat: refactor user migration

--- infra/migrations/1779732754461_create-users.js
+++ infra/migrations/1779732754461_create-users.js
@@ -19,18 +19,20 @@ export const up = (pgm) => {
     },
 
     password: {
-      type: "varchar(72)",
+      type: "varchar(60)",
       notNull: true,
     },
 
     created_at: {
       type: "timestamptz",
-      default: pgm.func("now()"),
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
     },
 
     updated_at: {
       type: "timestamptz",
-      default: pgm.func("now()"),
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
     },
   });
 };
